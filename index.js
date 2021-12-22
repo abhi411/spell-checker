a9e3ce1c7e13602a3b8ddf0a15be7fcee065897a
Ragini Sahu <ragsahu@coca-cola.com>
2021-12-22 20:56:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const fs = require('fs')
+var bodyParser = require('body-parser')
+const app = express()
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'views')));
+
+const port = 3000
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.post('/spellCheck', jsonParser, (req, res) => {
+    console.log("res",req.body)
+  res.send('Hello World!')
+})
+
+app.get('/', function (req, response, next) {
+    fs.readFile('./views/index.html', function (err, html) {
+        if (err) throw err;    
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
